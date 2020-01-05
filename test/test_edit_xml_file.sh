caf4cbdd3e7bc99b9fc2caa364ccaa7da0e01c00
‘vikilabs’ <vickyemailbox@gmail.com>
2020-01-05 23:18:27 +0100

    removed unused files

--- test/test_edit_xml_file.sh
+++ test/test_edit_xml_file.sh
@@ -1,12 +1,12 @@
-cp test.xml test_bkup.xml
+cp test.xml .##test_bkup.xml
 
 ../src/edit_xml_file "<debug>" "I am debuging" "test.xml"
 
-cp test.xml test_bkup.xml
+cp test.xml .##test_bkup.xml
 
 ../src/edit_xml_file "<debug>" "I am not debuging" "test.xml"
 
-cmp --silent test.xml test_bkup.xml
+cmp --silent test.xml .##test_bkup.xml
 
 if [ $? -eq 0 ]; then
     echo "[ ERROR ] edit_xml_file test failed"
@@ -15,11 +15,11 @@ else
     echo "[ SUCCESS] edit_xml_file test successful"
 fi
 
-cp test.xml test_bkup.xml
+cp test.xml .##test_bkup.xml
 
 ../src/edit_xml_file "<debug>" "I am debuging" "test.xml"
 
-cmp --silent test.xml test_bkup.xml
+cmp --silent test.xml .##test_bkup.xml
 
 if [ $? -eq 0 ]; then
     echo "[ ERROR ] edit_xml_file test failed"
@@ -28,3 +28,4 @@ else
     echo "[ SUCCESS] edit_xml_file test successful"
 fi
 
+rm .##test_bkup.xml
